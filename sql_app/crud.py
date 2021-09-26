47a10d4b70b2f551aa9142b5bc073679d53f8af5
Cristian <Cristianemmanuel22@gmail.com>
2021-09-25 22:37:40 -0600

    se cambio crud dejando las funciones de consultar,crear,borrar,actualizar,leer

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -4,32 +4,6 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 from sqlalchemy.exc import SQLAlchemyError
 
-def get_categoria(db: Session, categoria_id: int):
-    return db.\
-        query(models.Categoria).\
-        filter(models.Categoria.id == categoria_id).\
-        first()
-
-def get_categorias(db: Session, skip: int = 0, limite: int = 100):
-    return db.\
-        query(models.Categoria).\
-        offset(skip).\
-        limit(limite).\
-        all() # limit top
-
-# ==================================================================================================
-
-def get_editorial(db: Session, editorial_id: int):
-    return db.query(models.Editorial).filter(models.Editorial.id == editorial_id).all()
-
-def get_editoriales(db: Session, offset: int = 0, limite: int = 100):
-    return db.\
-        query(models.Editorial).\
-        offset(offset).\
-        limit(limite).\
-        all()
-
-# ======================================================================================================
 
 def get_libros(db: Session, offset: int = 0, limite: int = 100):
     return db.\
@@ -37,18 +11,6 @@ def get_libros(db: Session, offset: int = 0, limite: int = 100):
         offset(offset).\
         limit(limite).\
         all()
-
-def get_libros_by_autor(db: Session, id_autor: int ):
-    return db.query(models.Autor).filter(models.Autor.id == id_autor).first().libros
-
-def get_libros_by_editorial(db: Session, id_editorial: int ):
-    return db.query(models.Editorial).filter(models.Editorial.id == id_editorial).first().libros
-
-def get_libros_by_categoria(db: Session, id_categoria: int ):
-    return db.query(models.Categoria).filter(models.Categoria.id == id_categoria).first().libros
-#=======================================================================================================================
-
-# ============================================================================================================
 
 def crear_cliente(db: Session, nuevo_cliente: schemas.Cliente_Post):
     try:
