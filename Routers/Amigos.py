d5e53b079d56d6530b43458013b98676acef399b
DAVID GUERRA <dggonzalez@mides.gob.gt>
2021-09-26 00:23:18 -0600

    router amigos se agrego endpoint get all

--- Routers/Amigos.py
+++ Routers/Amigos.py
@@ -13,7 +13,15 @@ async def getIsAlive():
 @router.get("/amigos/{id_amigo}", response_model = List[schemas.Amigos_Get], status_code=status.HTTP_200_OK )
 def get_amigos(id_amigo:int, db: Session = Depends(main.get_db)):
     try:
-        amigo = crud.get_amigos(db, id_amigo)
+        amigo = crud.get_Amigos(db, id_amigo)
         return amigo
     except:
        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Amigos no encontrados")
+
+@router.get("/librosAll", response_model = List[schemas.Amigos_Get], status_code=status.HTTP_200_OK )
+def get_librosAll(offset: int = 0, limite: int = 100, db: Session = Depends(main.get_db)):
+    try:
+        libro = crud.get_Amigos_All(db, offset, limite)
+        return libro
+    except Exception as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Amigos no encontrados")
