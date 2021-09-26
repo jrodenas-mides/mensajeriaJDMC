27db568261a8a451b6071ce5a75d8374a3cbc04f
DAVID GUERRA <dggonzalez@mides.gob.gt>
2021-09-25 22:50:26 -0600

    se agrego prefijo auth

--- Routers/Amigos.py
+++ Routers/Amigos.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 
-router = APIRouter()
-
-
+router = APIRouter(prefix="/auth")
 
 @router.get("/isalive", tags=["Amigos"])
 async def getIsAlive():
--- Versiones/v1.py
+++ Versiones/v1.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
-from Routers import Amigos
+from Routers import Amigos, Contacto, Mensaje
 
-app_v1 = FastAPI()
+app_v1 = FastAPI(root_path="/auth")
 
 tags_metadata = [
-    {"name": "Amigos", "description": "Servicios para administrar los amigos"}
+    {"name": "Amigos", "description": "Servicios para administrar los amigos"},
+    {"name": "Contacto", "description": "Servicios para administrar los contactos"},
+    {"name": "Mensaje", "description": "Servicios para administrar los mensajes"}
 ]
 
 app_v1 = FastAPI(root_path="/v1", openapi_tags=tags_metadata)
 app_v1.include_router(Amigos.router)
+app_v1.include_router(Contacto.router)
+app_v1.include_router(Mensaje.router)
 
