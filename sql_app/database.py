492e429557529a9694f36d8cf6a3dbc29a5d7f35
jrodenas-mides <rodenas333@gmail.com>
2021-09-25 23:44:58 -0600

    DataBase y Models Mensajeria. JR 25.09.2021

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #String de Conexión
-SQLALCHEMY_DATABASE_URL = "postgresql://con_app_mensajeria:1234@127.0.0.1/mensajeria"
+SQLALCHEMY_DATABASE_URL = "postgresql://con_app_mensajeria:mensajeria@127.0.0.1/mensajeria"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- sql_app/models.py
+++ sql_app/models.py
@@ -4,59 +4,75 @@ from sqlalchemy.orm import relationship
 from .database import Base
 
 
-class Autor(Base):
-    __tablename__ = "autor"
-    id = Column(Integer, primary_key=True, index=True)
-    usuario = Column(String, unique=False, index=False)
-    apellido = Column(String, unique=False, index= True)
-    nacimiento = Column(Date, nullable=False)
-    pais = Column(String, unique=False, index=True, nullable=False)
-    fallecimiento = Column(Date, nullable=True)
+class Contactos(Base):
+    __tablename__ = "contactos"
+    Id = Column(Integer, primary_key=True, index=True)
+    Usuario = Column(String, index=False, nullable=False, unique=True)
+    Nombres  = Column(String, unique=False, index= True, nullable=False)
+    Apellidos  = Column(String, nullable=False)
+    CorreoElectronico  = Column(String, unique=True, index=True, nullable=False, )
+    Telefono  = Column(Date, nullable=True)
+    Genero  = Column(Date, nullable=False)
+    FechaNacimiento  = Column(Date, nullable=False)
+    Jwtoken  = Column(String, nullable=False)
+    IntentosFallidos  = Column(Integer, nullable=False)
+    FechaBloqueo  = Column(Date, nullable=False)
+    IdRol  = Column(Integer, ForeignKey("roles.id") ,nullable=False)
 
-    libros = relationship("Libro", back_populates = "autor")
 
+    roles = relationship("Rol", back_populates = "contactos")
+    contactos =  relationship("Amigos", back_populates = "contactos")
+    estatus = relationship("Estatus", back_populates="contactos")
 
 # ============================================================================================
-class Categoria(Base):
-    __tablename__ = "categoria"
-    id = Column(Integer, primary_key=True, index=True) # Higher Order Functions: Funcions que reciben una function como parametro y devuelven otra función como parametro
-    nombre = Column(String, unique=True, index=True)
-    libros = relationship("Libro", back_populates="categoria")
+class Amigos(Base):
+    __tablename__ = "amigos"
+    Id = Column(Integer, primary_key=True, index=True)
+    Id_Contacto1 = Column(Integer, ForeignKey("contactos.id"), unique=False, index=True, nullable=False)
+    Id_Contacto2 = Column(Integer,  ForeignKey("contactos.id"), unique=False, index=True, nullable=False)
+    Id_Estatus = Column(Integer,  ForeignKey("estatus.id") ,nullable=False)
+    Fecha_Asociacion  = Column(Date, nullable=False)
+    Llave_Cifrada  = Column(String, nullable=True)
+
+    contactos = relationship("Contactos", back_populates="amigos")
+    estatus =  relationship("Estatus", back_populates="amigos")
 
 # ====================================================================================================
 
-class Editorial(Base):
-    __tablename__ = "editorial"
-    id = Column(Integer, primary_key = True, index=True)
-    nombre = Column(String, unique = True, index = True)
-    pais = Column(String)
+class Mensajes(Base):
+    __tablename__ = "mensajes"
+    Id = Column(Integer, primary_key = True, index=True)
+    Id_Emisor  = Column(Integer,  ForeignKey("contactos.id") , index = True)
+    Id_Receptor  = Column(String,  ForeignKey("contactos.id") , index = True)
+    Fecha_Envio  = Column(Date, nullable=False)
+    Id_Estatus  = Column(Integer,  ForeignKey("estatus.id") ,unique=True, index=True)
+    Mensaje = Column(String)
 
-    libros = relationship("Libro", back_populates="editorial")
+
+    contactos = relationship("Contactos", back_populates="mensajes")
+    estatus =  relationship("Estatus", back_populates="mensajes")
 
 # ====================================================================================================
 
-class Libro(Base):
-    __tablename__ = "libro"
-    id = Column(Integer, primary_key= True, index = True)
-    nombre = Column(String, unique= True, index = True)
-    id_autor = Column(Integer, ForeignKey("autor.id"), index=True)
-    id_categoria = Column(Integer, ForeignKey("categoria.id"), index=True)
-    precio = Column(Float, nullable=False)
-    edicion = Column(String, nullable=True)
-    id_editorial = Column(Integer, ForeignKey("editorial.id"), index=True)
-    anio_impresion = Column(Integer, nullable=False)
-
-    autor = relationship("Autor", back_populates="libros")
-    categoria = relationship("Categoria", back_populates="libros")
-    editorial = relationship("Editorial", back_populates="libros")
+class Estatus(Base):
+    __tablename__ = "estatus"
+    Id = Column(Integer, primary_key = True, index=True)
+    Id_Categoria   = Column(Integer,  ForeignKey("categoriasestatus.id") ,index = True)
+    Descripcion   = Column(String, index = True, unique=True)
 
 
-# ==========================================================================================================
+    categoriasestatus = relationship("categoriasestatus", back_populates="estatus")
 
-class Cliente(Base):
-    __tablename__ = "cliente"
-    id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, nullable=False)
-    apellidos = Column(String, nullable=False, index=True)
-    correo = Column(String, nullable=False, unique=True, index=True)
+class CategoriasEstatus(Base):
+    __tablename__ = "categoriasestatus"
+    Id = Column(Integer, primary_key = True, index=True)
+    Descripcion   = Column(String, index = True, unique=True)
 
+
+class Roles(Base):
+    __tablename__ = "roles"
+    Id = Column(Integer, primary_key=True, index=True)
+    Nombre_Rol = Column(String, index=True, unique=True)
+    Id_Estatus = Column(Integer,  ForeignKey("estatus.id"))
+
+    estatus = relationship("Estatus", back_populates="roles")
